8de754724fb8ace9344fe6c73621bd16cb51a945
Henrique da Silva <henriquesilvarosa80@gmail.com>
2023-09-05 21:30:06 -0300

    HenriueAtualizacao(SALVAR E DELETAR) ;

--- code/tcheFood/src/br/com/tchefood/DAO/CategoriaDAO.java
+++ code/tcheFood/src/br/com/tchefood/DAO/CategoriaDAO.java
@@ -3,17 +3,23 @@ package br.com.tchefood.DAO;
 import br.com.tchefood.banco.ConexaoMysql;
 import br.com.tchefood.model.CategoriaModel;
 
+import java.rmi.server.ExportException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CategoriaDAO {
     public void salvar(CategoriaModel usuario) throws SQLException, ClassNotFoundException {
+    try {
         ConexaoMysql conexaoMysql = new ConexaoMysql();
         Connection conn = conexaoMysql.obterConexao();
         PreparedStatement stmt = null;
-        stmt = conn.prepareStatement("INSERT INTO tb_categoria_produto(descricao) VALUES (?,?,?,?,?)");
+        stmt = conn.prepareStatement("INSERT INTO tb_categoria_produto(descricao) VALUES (?)");
         stmt.setString(1, usuario.getDescricao());
         stmt.executeUpdate();
+    } catch (Exception e1){
+        System.err.println(e1.getMessage());
+
+    }
     }
 }
--- code/tcheFood/src/br/com/tchefood/view/CategoriaSalvarProduto.java
+++ code/tcheFood/src/br/com/tchefood/view/CategoriaSalvarProduto.java
@@ -1,11 +1,14 @@
 package br.com.tchefood.view;
 
 import br.com.tchefood.DAO.CategoriaDAO;
+import br.com.tchefood.banco.ConexaoMysql;
 import br.com.tchefood.model.CategoriaModel;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class CategoriaSalvarProduto {
@@ -59,11 +62,13 @@ public class CategoriaSalvarProduto {
     private JButton SALVARButton;
     private JLabel JLnomeProduto;
     private JButton EXCLUIRButton;
+    private JTextField textField2;
 
     public CategoriaSalvarProduto() {
     SALVARButton.addActionListener(new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
+            System.out.println("PASSOU AQUI");
             CategoriaDAO categoriaDAO = new CategoriaDAO();
             CategoriaModel categoria = new CategoriaModel();
             categoria.setDescricao(textField1.getText());
@@ -79,8 +84,55 @@ public class CategoriaSalvarProduto {
         EXCLUIRButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                CategoriaDAO categoriaDAO = new CategoriaDAO();
+                CategoriaModel categoria = new CategoriaModel();
+                ConexaoMysql connection = new ConexaoMysql();
+                categoria.setId(textField2.getInt);
 
+                Connection conn;
+                try {
+                    conn = connection.obterConexao();
+                } catch (ClassNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                PreparedStatement stmt = null;
+                try {
+                    stmt = conn.prepareStatement("DELETE FROM tb_categoria_produto WHERE id = ?");
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    stmt.setInt(1, categoria.getId());
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+                try {
+                    stmt.executeUpdate();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    conn.close();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    stmt.close();
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CategoriaSalvarProduto");
+        frame.setContentPane(new CategoriaSalvarProduto().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
