28ae402269fcbf8855d22a5b143d48f883568e4f
Your Name <henriquesilvarosa80@gmail.com>
2023-09-05 21:49:50 -0300

    chico atualizando

---
+++ code/tcheFood/src/br/com/tchefood/view/GerenciamentoProduto.java
@@ -0,0 +1,43 @@
+package br.com.tchefood.view;
+
+import br.com.tchefood.model.ProdutoModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GerenciamentoProduto {
+    private JButton salvarButton;
+    private JComboBox comboBoxCategoria;
+    private JTextField tfDescricao;
+    private JTextField tfProduto;
+    private JLabel jlDescricao;
+    private JLabel jlCategoria;
+    private JLabel jlProduto;
+    private JPanel jpGerenciamentoProduto;
+    public GerenciamentoProduto() {
+        comboBoxCategoria.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox <String> comboBoxCategoria = new JComboBox<>();
+
+                comboBoxCategoria.addItem("Pizza");
+                comboBoxCategoria.addItem("Xis");
+                comboBoxCategoria.addItem("Batata frita");
+
+                ProdutoModel produto = new ProdutoModel();
+                produto.setValor(Float.parseFloat(tfProduto.getText()));
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("GerenciamentoProduto");
+        frame.setContentPane(new GerenciamentoProduto().jpGerenciamentoProduto);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+
