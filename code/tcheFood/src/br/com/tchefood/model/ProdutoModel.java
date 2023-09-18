a10f7a44d35eb5ac25e61a362968f1837e1ee4cb
Francisco Feix <111092720+francisco0421@users.noreply.github.com>
2023-09-18 16:46:52 -0300

    updating dao

--- code/tcheFood/src/br/com/tchefood/model/ProdutoModel.java
+++ code/tcheFood/src/br/com/tchefood/model/ProdutoModel.java
@@ -4,9 +4,25 @@ public class ProdutoModel {
     private int id;
     private String descricao;
     private float preco;
+    private String CategoriaProduto;
 
     private CategoriaModel categoria;
 
+    public String getCategoriaProduto() {
+        return CategoriaProduto;
+    }
+
+    public void setCategoriaProduto(String categoriaProduto) {
+        CategoriaProduto = categoriaProduto;
+    }
+
+    public CategoriaModel getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaModel categoria) {
+        this.categoria = categoria;
+    }
 
     public int getId() {
         return id;
